eeadb90a198f63740160a651a7eca913ac2bbfea
Jaroslav Brtis <jaroslav.brtis@gmail.com>
2024-07-30 20:58:14 -0400

    Fixed plugin setup.

--- simple-time-tracker-plugin/src/main/java/com/jardoapps/timesheet/plugin/stt/SimpleTaskTrackerPlugin.java
+++ simple-time-tracker-plugin/src/main/java/com/jardoapps/timesheet/plugin/stt/SimpleTaskTrackerPlugin.java
@@ -1,47 +1,55 @@
 package com.jardoapps.timesheet.plugin.stt;
 
+import org.pf4j.Extension;
+import org.pf4j.Plugin;
+
 import com.jardoapps.timesheet.plugin.api.TimesheetFillerPlugin;
 
-public class SimpleTaskTrackerPlugin implements TimesheetFillerPlugin {
+public class SimpleTaskTrackerPlugin extends Plugin {
 
-	@Override
-	public boolean supportsLoading() {
-		return true;
-	}
+	@Extension
+	public static class SimpleTaskTrackerPluginExtension implements TimesheetFillerPlugin {
 
-	@Override
-	public boolean supportsTransforming() {
-		return false;
-	}
+		@Override
+		public boolean supportsLoading() {
+			return true;
+		}
 
-	@Override
-	public boolean supportsSaving() {
-		return false;
-	}
+		@Override
+		public boolean supportsTransforming() {
+			return false;
+		}
 
-	@Override
-	public String getName() {
-		return "Simple Task Tracker App";
-	}
+		@Override
+		public boolean supportsSaving() {
+			return false;
+		}
 
-	@Override
-	public String getDescription() {
-		return "Loads records exported from Simple Task Tracker application.";
-	}
+		@Override
+		public String getName() {
+			return "Simple Task Tracker App";
+		}
 
-	@Override
-	public RecordLoader getLoader() {
-		return new SttRecordLoader();
-	}
+		@Override
+		public String getDescription() {
+			return "Loads records exported from Simple Task Tracker application.";
+		}
 
-	@Override
-	public RecordTransformer getTransformer() {
-		throw new UnsupportedOperationException("SimpleTaskTrackerPlugin does not support transforming.");
-	}
+		@Override
+		public RecordLoader getLoader() {
+			return new SttRecordLoader();
+		}
 
-	@Override
-	public RecordSaver getSaver() {
-		throw new UnsupportedOperationException("SimpleTaskTrackerPlugin does not support saving.");
+		@Override
+		public RecordTransformer getTransformer() {
+			throw new UnsupportedOperationException("SimpleTaskTrackerPlugin does not support transforming.");
+		}
+
+		@Override
+		public RecordSaver getSaver() {
+			throw new UnsupportedOperationException("SimpleTaskTrackerPlugin does not support saving.");
+		}
+
 	}
 
 }
